0ee5bcd151e1db31bfafdf6289a06f1e7d65b699
blurbks-bit <blurb.ks@gmail.com>
2026-05-05 11:11:54 +0200

    Clean repo structure and add GTM

--- index.php
+++ index.php
@@ -920,6 +920,13 @@ $schema = [
 <!doctype html>
 <html lang="<?= e($currentLang); ?>">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KL5ZDQQJ');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= e($metaTitle); ?></title>
@@ -940,6 +947,10 @@ $schema = [
     <script type="application/ld+json"><?= json_encode($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES); ?></script>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KL5ZDQQJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header class="site-header" data-header>
     <div class="top-strip">
         <div class="container top-strip__inner">
